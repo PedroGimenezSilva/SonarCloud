cbfa4f0316c6000ce00ba6cdc11b504f0bbf7635
PedroGimenezSilva <pedro.msilva@bandtec.com.br>
2023-10-15 12:56:41 -0300

    ci:fix-sonarcloud coverage

--- sum.go
+++ sum.go
@@ -11,22 +11,22 @@ func sum(a int, b int) int {
 }
 
 
-func sub(a int, b int) int {
-	return a - b
-}
+// func sub(a int, b int) int {
+// 	return a - b
+// }
 
 
 
-func times(a int, b int) int {
-	return a * b
-}
+// func times(a int, b int) int {
+// 	return a * b
+// }
 
 
-func sumX(a int, b int) int {
-	return a + b + a
-}
+// func sumX(a int, b int) int {
+// 	return a + b + a
+// }
 
 
-func sumY(a int, b int) int {
-	return a + b + a + b
-}
+// func sumY(a int, b int) int {
+// 	return a + b + a + b
+// }
